e154e3d5fe6281a8bf25473fe385ca7c73854b49
igorMandala <igor@mandalaclothing.com.br>
2022-03-20 22:33:22 -0300

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <Tutorial />
+  <div class="flex justify-center text-center w-full">
+    <div class="text-7xl">TICK TICK PLEY TICK PLAW CHARLIE BROW</div>
+  </div>
 </template>
 
 <script lang="ts">
