01da15ded2f2f66030920674f4d1a5e05db72892
Igor <igornascimento02@gmail.com>
2023-05-13 00:28:50 -0300

    mobile support

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  MOBILE_STRING: string[] = ['Android', 'iPhone'];
+
   constructor() { }
+
+  getImage() {
+    if (!!this.MOBILE_STRING.filter(option => navigator.userAgent.includes(option)).length) {
+      console.log(navigator.userAgent)
+      return '/assets/images/sm-main-banner.png';
+    }
+
+    return '/assets/images/lg-main-banner.png';
+  }
 }
